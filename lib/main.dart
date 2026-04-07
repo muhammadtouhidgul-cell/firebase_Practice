e1c62cbb64632de3272eada819173d18d63652f8
Touheed-Niazi <muhammadtouhidgul@gmail.com>
2026-04-07 04:24:11 -0700

    Checking the commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,20 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_wscube/home_screen/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
-void main() async{
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    print('Updated');
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
